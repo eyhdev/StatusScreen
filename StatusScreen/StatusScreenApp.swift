19e1050be2dbe3a1ffbeb5f2ef211b10a686fc3e
eyhdev <alaqsa335@gmail.com>
2023-09-15 13:56:01 +0300

    Initial Commit

---
+++ StatusScreen/StatusScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusScreenApp.swift
+//  StatusScreen
+//
+//  Created by eyh.mac on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct StatusScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
